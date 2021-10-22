b813a42b1724288950b7e8a7f427107ed137a1f3
João Marcos Alves de Aguiar <jmaraguiar22@gmail.com>
2021-10-22 17:16:26 -0300

    Tasklist 1.2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var addButton = document.getElementById("add");
 var redefList = document.getElementById("redef");
 var val = document.getElementById("txt");
 var list = [];
+var tagged = false;
 
 function active(){
     let input= document.getElementById("txt").value;
@@ -37,13 +38,14 @@ function showList(){
 
     for(item of list){
 
+        var pos = list.indexOf(item);
+
         let element = document.createElement('li');
         let textItem = document.createTextNode(item);
         element.appendChild(textItem);
         ul.appendChild(element);
+        element.setAttribute('onclick','taskComplete(this)')
     
-
-        let pos = list.indexOf(item);
 
         let bt = document.createElement('button');
         bt.innerHTML = "DELETE";
@@ -76,6 +78,22 @@ function delAll(){
     location.reload();
 
 }
+
+function taskComplete(item){
+
+    if (!tagged){
+        item.style.backgroundColor = 'rgba(70, 70, 70, 0.205)'
+        item.style.textDecoration = 'line-through'
+        tagged = true
+    }
+    else {
+        item.style.backgroundColor = 'white'
+        item.style.textDecoration = 'none'
+        tagged = false
+    }
+
+}
+
 
 
 
